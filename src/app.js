2c70d4de21532733c159ab3106d389dc448a8862
Aditya Paliwal <paliwaladitya298@gmail.com>
2026-09-22 18:40:47 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,13 +2,35 @@ const express = require("express")
 
 const app = express()
 
-app.use( "/test" , (req , res)=>{
-    res.send("hjkdf")
+app.get("/a*c", (req , res)=>{
+    res.send("a aur b ke bich me kuch bhi likho ")
 })
- app.use( "/te" , (req , res)=>{
-    res.send("hjkdf")
+
+app.get("/a{b}c", (req , res)=>{
+    res.send("ignore b ")
 })
-app.listen(3 , ()=>{
+
+// app.get( /a/ , (req , res)=>{
+//     res.send("URL me kahi par bhi a aaya toh chlega")
+// })
+
+app.get(/.*fly$/ , (req , res)=>{
+    res.send("kuch bhi likho par ankhri me fly hua to chale ga ")
+})
+
+// URL me se kisi chiz ko extract krna ho to req.query likh te hai (suppose browser pe kisi ne likha hoga  http://localhost:3000/user?UserId=90&token=69)
+app.get("/user", (req , res)=>{
+    console.log(req.query);
+    res.send("hello")
+})
+
+app.get("/user/:userId/:name/:age" ,(req , res)=>{
+    console.log(req.params);
+    
+    res.send("learning req.params")
+})
+
+app.listen(3000 , ()=>{
     console.log("server is running");
     
 })
